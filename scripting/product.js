6086e20682d87746fa8ffeba5f272010a7ed0b2f
okuhle gxotiwe <gxotiweokuhle@gmail.com>
2023-06-15 08:25:53 +0200

    ok

--- scripting/product.js
+++ scripting/product.js
@@ -164,7 +164,7 @@ products.forEach((product, id) => {
 });
 
 let cart = JSON.parse(window.localStorage.getItem("Products")) || [];//add to second local storage
-if()
+
 
 function addToCart(productId) {
   const product = products.find((product) => product.id === productId);
